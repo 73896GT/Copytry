a7a14bb4411c0715a345fd78de696323aabc9b76
1 <lllkolotalllep@gmail.com>
2020-12-18 15:42:09 +0300

    intl

---
+++ Copytry/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Copytry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
